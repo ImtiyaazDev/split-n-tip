fdff00a87e2cf22674066bb0e893349f90f57c80
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 16:53:07 +0200

    refactor: Remove unrequired div

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,22 +6,18 @@ import Tips from "./Tips";
 export default function Main() {
 	return (
 		<main className="col-span-full flex h-full w-full flex-col gap-6">
-			<div className="flex h-auto flex-col gap-6">
-				<ButtonGroup />
-				<div className="flex flex-col gap-4">
-					<h2 className="font-heading text-2xl text-stone-700">Bill amount</h2>
-					<FriendMeals />
-				</div>
-				<div className="flex flex-col gap-4">
-					<h2 className="font-heading text-2xl text-stone-700">Tips</h2>
-					<Tips />
-				</div>
-				<div className="flex flex-col gap-4">
-					<h2 className="font-heading text-2xl text-stone-700">
-						Split Payments
-					</h2>
-					<Payments />
-				</div>
+			<ButtonGroup />
+			<div className="flex flex-col gap-4">
+				<h2 className="font-heading text-2xl text-stone-700">Bill amount</h2>
+				<FriendMeals />
+			</div>
+			<div className="flex flex-col gap-4">
+				<h2 className="font-heading text-2xl text-stone-700">Tips</h2>
+				<Tips />
+			</div>
+			<div className="flex flex-col gap-4">
+				<h2 className="font-heading text-2xl text-stone-700">Split Payments</h2>
+				<Payments />
 			</div>
 		</main>
 	);
