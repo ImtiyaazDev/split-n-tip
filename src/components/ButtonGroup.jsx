d2bbf42db602c29c6a530abb645afe58922ece25
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 11:15:09 +0200

    style: Explicitly position button group in first row

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 export default function ButtonGroup() {
 	return (
-		<div className="flex items-start justify-between">
+		<div className="row-start-1 row-end-1 flex items-start justify-between">
 			<Button type="primary">Add Payee</Button>
 			<Button type="secondary">New Bill</Button>
 		</div>
