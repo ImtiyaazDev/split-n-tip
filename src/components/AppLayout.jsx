b50029ffa80f9c16760bc4bca50fadfb42890c01
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 16:50:56 +0200

    style: Adjust padding for 768px breakpoint

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -4,7 +4,7 @@ import Main from "./Main";
 
 export default function AppLayout() {
 	return (
-		<div className="grid h-screen w-screen grid-cols-6 grid-rows-[6rem_1fr_4rem] place-items-center overflow-y-auto bg-stone-100 p-5">
+		<div className="grid h-screen w-screen grid-cols-6 grid-rows-[6rem_1fr_4rem] place-items-center overflow-y-auto bg-stone-100 p-5 md:px-14 md:py-10">
 			<Header />
 			<Main />
 			<Footer />
