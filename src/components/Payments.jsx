39f69e7b945d1953b20f2f60e9e4bafa0ad7e009
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 12:20:53 +0200

    feat: Add payments section

---
+++ src/components/Payments.jsx
@@ -0,0 +1,16 @@
+import PriceTag from "./PriceTag";
+
+export default function Payments() {
+	return (
+		<div className="rounded-2xl bg-violet-200 p-5 space-y-6">
+			<div className="flex items-center justify-between border-b border-b-violet-300 py-2.5">
+				<p className="text-lg font-bold text-stone-700">Imtiyaaz pays</p>
+				<PriceTag />
+			</div>
+			<div className="flex items-center justify-between">
+				<p className="text-xl font-bold text-violet-800">Total</p>
+				<PriceTag />
+			</div>
+		</div>
+	);
+}
