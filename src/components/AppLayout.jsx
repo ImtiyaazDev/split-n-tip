5b093656ae29906e3001a700923cf77a6ba9813c
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 09:20:39 +0200

    style: Change app background colour

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function AppLayout() {
 	return (
-		<div className="grid h-screen w-screen grid-cols-6 p-5 grid-rows-[6rem_1fr] place-items-center">
+		<div className="grid h-screen w-screen grid-cols-6 grid-rows-[6rem_1fr] place-items-center p-5 bg-stone-100">
 			<Header />
 		</div>
 	);
