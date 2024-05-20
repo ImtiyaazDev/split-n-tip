bd4a0a189750e7054d24878dd7ee9af4d2a73b93
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 12:30:47 +0200

    style: Style footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+	return (
+		<p className="font-heading col-span-full text-stone-600">
+			Designed and built by{" "}
+			<a
+				href="https:github.com/Imtiyaazdev"
+				target="_blank"
+				className="transition-colors duration-300 hover:text-violet-500"
+			>
+				Imtiyaaz Isaacs
+			</a>
+		</p>
+	);
+}
