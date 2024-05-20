837ae7900bfa7bbaa07e326a9fe9ff94f2c01c08
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 16:35:59 +0200

    style: Add secondary headings for each section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,13 +6,22 @@ import Tips from "./Tips";
 export default function Main() {
 	return (
 		<main className="col-span-full grid h-full w-full grid-rows-2 gap-y-6">
-			<div className="flex flex-col gap-6 row-start-1 row-end-1 h-auto">
+			<div className="row-start-1 row-end-1 flex h-auto flex-col gap-6">
 				<ButtonGroup />
-				<FriendMeals />
+				<div className="flex flex-col gap-4">
+					<h2 className="font-heading text-2xl text-stone-700">Bill amount</h2>
+					<FriendMeals />
+				</div>
 			</div>
-			<div className="flex flex-col gap-6 row-start-2">
-				<Tips />
-				<Payments />
+			<div className="row-start-2 flex flex-col gap-6">
+				<div className="flex flex-col gap-4">
+					<h2 className="font-heading text-2xl text-stone-700">Tips</h2>
+					<Tips />
+				</div>
+				<div className="flex flex-col gap-4">
+					<h2 className="font-heading text-2xl text-stone-700">Split Payments</h2>
+					<Payments />
+				</div>
 			</div>
 		</main>
 	);
