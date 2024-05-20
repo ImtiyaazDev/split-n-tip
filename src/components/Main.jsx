66e1e61bfab442854d756fabb96456dd60a8c17f
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 09:46:26 +0200

    feat: Create main layout for app

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+export default function Main() {
+	return <main className="col-span-full h-full w-full bg-red-200 grid grid-rows-4">
+    <div>
+      <button>Add new payee</button>
+      <button>New bill</button>
+    </div>
+    <div>
+      {/* Meals */}
+      <h2>Friends List</h2>
+    </div>
+    <div>
+      <h2>tips</h2>
+    </div>
+    <div>
+      <h2>Payments</h2>
+    </div>
+  </main>;
+}
