8b33bcef2d0f09c4940ec76f6064b4b5b0fd0433
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 16:42:37 +0200

    style: Change layout type from grid to flex

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,21 +5,21 @@ import Tips from "./Tips";
 
 export default function Main() {
 	return (
-		<main className="col-span-full grid h-full w-full grid-rows-2 gap-y-6">
-			<div className="row-start-1 row-end-1 flex h-auto flex-col gap-6">
+		<main className="col-span-full flex h-full w-full flex-col gap-6">
+			<div className="flex h-auto flex-col gap-6">
 				<ButtonGroup />
 				<div className="flex flex-col gap-4">
 					<h2 className="font-heading text-2xl text-stone-700">Bill amount</h2>
 					<FriendMeals />
 				</div>
-			</div>
-			<div className="row-start-2 flex flex-col gap-6">
 				<div className="flex flex-col gap-4">
 					<h2 className="font-heading text-2xl text-stone-700">Tips</h2>
 					<Tips />
 				</div>
 				<div className="flex flex-col gap-4">
-					<h2 className="font-heading text-2xl text-stone-700">Split Payments</h2>
+					<h2 className="font-heading text-2xl text-stone-700">
+						Split Payments
+					</h2>
 					<Payments />
 				</div>
 			</div>
