52567ffce3bfb0be229a40ef693ae878383f109e
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 12:31:07 +0200

    style: Adjust overall layout

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,9 +1,13 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import Main from "./Main";
 
 export default function AppLayout() {
 	return (
-		<div className="grid h-screen w-screen grid-cols-6 grid-rows-[6rem_1fr] place-items-center p-5 bg-stone-100">
+		<div className="grid h-screen w-screen grid-cols-6 grid-rows-[6rem_1fr_4rem] place-items-center overflow-y-auto bg-stone-100 p-5">
 			<Header />
+			<Main />
+			<Footer />
 		</div>
 	);
 }
