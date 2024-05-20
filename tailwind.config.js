7844fc2471724186a22a0710318aa2f3fb3101aa
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-20 09:00:21 +0200

    config: Add custom fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		fontFamily: {
+			sans: "Nokora, Helvetica, Arial, sans-serif"
+		},
+		extend: {
+			fontFamily: {
+				heading: "Paytone One, Helvetica, Arial, sans-serif"
+			}
+		}
+	},
+	plugins: []
+};
