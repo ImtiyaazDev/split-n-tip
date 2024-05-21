e6458ca13afa1d14f70a39225379faa530d58383
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-21 16:09:30 +0200

    feat: Create redux slice for modal state

---
+++ src/slices/modalSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	isOpen: false
+};
+
+const modalSlice = createSlice({
+	name: "modal",
+	initialState,
+	reducers: {
+		showModal: (state, action) => (state.isOpen = action.payload)
+	}
+});
+
+export const { showModal } = modalSlice.actions;
+export default modalSlice.reducer;
