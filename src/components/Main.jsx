6ba1813265be5df44d2d6613ae3014979cd716bc
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-21 11:17:17 +0200

    style: Adjust layout for 768px breakpoint

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,19 +5,25 @@ import Tips from "./Tips";
 
 export default function Main() {
 	return (
-		<main className="col-span-full flex h-full w-full flex-col gap-6">
-			<ButtonGroup />
-			<div className="flex flex-col gap-4">
-				<h2 className="font-heading text-2xl text-stone-700">Bill amount</h2>
-				<FriendMeals />
+		<main className="col-span-full flex h-full w-full flex-col gap-6 md:flex-row md:justify-between">
+			<div className="md:grow md:flex md:flex-col md:gap-4">
+				<ButtonGroup />
+				<div className="flex flex-col gap-4">
+					<h2 className="font-heading text-2xl text-stone-700">Bill amount</h2>
+					<FriendMeals />
+				</div>
 			</div>
-			<div className="flex flex-col gap-4">
-				<h2 className="font-heading text-2xl text-stone-700">Tips</h2>
-				<Tips />
-			</div>
-			<div className="flex flex-col gap-4">
-				<h2 className="font-heading text-2xl text-stone-700">Split Payments</h2>
-				<Payments />
+			<div className="md:grow md:flex md:flex-col md:gap-4">
+				<div className="flex flex-col gap-4">
+					<h2 className="font-heading text-2xl text-stone-700">Tips</h2>
+					<Tips />
+				</div>
+				<div className="flex flex-col gap-4">
+					<h2 className="font-heading text-2xl text-stone-700">
+						Split Payments
+					</h2>
+					<Payments />
+				</div>
 			</div>
 		</main>
 	);
