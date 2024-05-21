2cf239ed3ec37f9826ea6d2ad8d7ec94cb78090b
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-21 16:52:26 +0200

    feat: Update button for form submission

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,6 +9,16 @@ export default function Button({ type, onClick, children }) {
 		cancel: `bg-red-500 text-red-100 hover:bg-red-600 focus:ring-red-500 rounded-full px-6 py-2 font-bold outline-none transition-colors duration-300 focus:ring-2 focus:ring-offset-2`
 	};
 
+	if (type === "form")
+		return (
+			<button
+				type="submit"
+				className={styles[type]}
+			>
+				{children}
+			</button>
+		);
+
 	return (
 		<button
 			className={styles[type]}
