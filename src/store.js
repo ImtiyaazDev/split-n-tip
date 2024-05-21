24cba363c701e813477510e5888f69e2c3a63579
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-21 16:43:20 +0200

    feat: Add bill slice to store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import billReducer from "./slices/billSlice";
 import modalReducer from "./slices/modalSlice";
 
 export const store = configureStore({
 	reducer: {
-		modal: modalReducer
+		modal: modalReducer,
+		bill: billReducer
 	}
 });
