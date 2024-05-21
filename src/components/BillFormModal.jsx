a81f64745beb53e5ac28ed9f7698d47a53a112cd
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-21 16:34:31 +0200

    feat: Add form cancel button

--- src/components/BillFormModal.jsx
+++ src/components/BillFormModal.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 export default function BillFormModal() {
 	return (
-		<form className="col-span-full flex w-1/2 h-2/3 flex-col items-center justify-evenly gap-4 rounded-2xl bg-stone-300 p-5">
+		<form className="col-span-full flex h-2/3 flex-col items-center justify-evenly gap-4 rounded-2xl bg-stone-300 p-5">
 			<h2 className="font-heading text-2xl text-stone-700">Billing Details</h2>
 			<div className="flex flex-col gap-4">
 				<div className="flex items-center justify-between gap-7">
@@ -21,7 +21,10 @@ export default function BillFormModal() {
 						className="h-8 w-40 rounded-2xl px-4"
 					/>
 				</div>
-				<Button type='form'>Submit</Button>
+				<div className="flex justify-evenly">
+					<Button type="form">Submit</Button>
+					<Button type="cancel">Cancel</Button>
+				</div>
 			</div>
 		</form>
 	);
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,16 @@ export default function Button({ type, onClick, children }) {
 	const styles = {
 		primary: `${base} bg-violet-500 text-violet-100 hover:bg-violet-600 focus:ring-violet-500`,
 		secondary: `${base} bg-violet-200 text-violet-700 hover:bg-violet-500 hover:text-violet-100 focus:ring-violet-600`,
-		form: `bg-violet-500 text-violet-100 hover:bg-violet-600 focus:ring-violet-500 rounded-full px-6 py-2 font-bold outline-none transition-colors duration-300 focus:ring-2 focus:ring-offset-2`
+		form: `bg-violet-500 text-violet-100 hover:bg-violet-600 focus:ring-violet-500 rounded-full px-6 py-2 font-bold outline-none transition-colors duration-300 focus:ring-2 focus:ring-offset-2`,
+		cancel: `bg-red-500 text-red-100 hover:bg-red-600 focus:ring-red-500 rounded-full px-6 py-2 font-bold outline-none transition-colors duration-300 focus:ring-2 focus:ring-offset-2`
 	};
 
-	return <button className={styles[type]}>{children}</button>;
+	return (
+		<button
+			className={styles[type]}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
 }
