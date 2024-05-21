008034c622ccd329a4184bc2af39217aa5811719
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-21 11:36:10 +0200

    feat: Add theme icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+
 import ButtonGroup from "./ButtonGroup";
 import FriendMeals from "./FriendMeals";
 import Payments from "./Payments";
@@ -6,14 +8,18 @@ import Tips from "./Tips";
 export default function Main() {
 	return (
 		<main className="col-span-full flex h-full w-full flex-col gap-6 md:flex-row md:justify-between">
-			<div className="md:grow md:flex md:flex-col md:gap-4">
+			<div className="md:flex md:grow md:flex-col md:gap-4">
 				<ButtonGroup />
 				<div className="flex flex-col gap-4">
 					<h2 className="font-heading text-2xl text-stone-700">Bill amount</h2>
 					<FriendMeals />
 				</div>
 			</div>
-			<div className="md:grow md:flex md:flex-col md:gap-4">
+			<div className="md:flex md:grow md:flex-col md:gap-4">
+				<div className="flex w-fit items-center gap-6 self-end rounded-full bg-violet-500 px-6 py-3">
+					<SunIcon className="size-7 cursor-pointer text-violet-100" />
+					<MoonIcon className="size-7 cursor-pointer text-violet-700" />
+				</div>
 				<div className="flex flex-col gap-4">
 					<h2 className="font-heading text-2xl text-stone-700">Tips</h2>
 					<Tips />
