c8dea22246146add40ab2a9d17d9544bf14a8755
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 11:45:01 +0200

    feat: Add total payment for each person

--- src/components/Payments.jsx
+++ src/components/Payments.jsx
@@ -1,12 +1,24 @@
+import { useSelector } from "react-redux";
 import PriceTag from "./PriceTag";
+import SinglePayment from "./SinglePayment";
 
 export default function Payments() {
+	const users = useSelector((store) => store.bill.users);
+	const tipAmountPerPerson = useSelector(
+		(store) => store.bill.tipAmountPerPerson
+	);
+
 	return (
 		<div className="h-fit space-y-6 rounded-2xl bg-violet-200 p-5">
-			<div className="flex items-center justify-between border-b border-b-violet-300 py-2.5">
-				<p className="text-lg font-bold text-stone-700">Imtiyaaz pays</p>
-				<PriceTag />
-			</div>
+			<ul>
+				{users.map((user) => (
+					<SinglePayment
+						key={user.id}
+						name={user.name}
+						amount={user.amount + tipAmountPerPerson}
+					/>
+				))}
+			</ul>
 			<div className="flex items-center justify-between">
 				<p className="text-xl font-bold text-violet-800">Total</p>
 				<PriceTag />
