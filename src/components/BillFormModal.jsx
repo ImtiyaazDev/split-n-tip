634764e7d2cf7a1fd0875ffb81e32414ad481fe1
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 12:05:44 +0200

    style: Move heading out of form and resize form container

--- src/components/BillFormModal.jsx
+++ src/components/BillFormModal.jsx
@@ -34,42 +34,44 @@ export default function BillFormModal() {
 	};
 
 	return (
-		<form
-			className="col-span-full flex h-2/3 flex-col items-center justify-evenly gap-4 rounded-2xl bg-stone-200 p-5"
-			onSubmit={handleSubmit}
-		>
+		<>
 			<h2 className="font-heading text-2xl text-stone-700">Billing Details</h2>
-			<div className="flex flex-col gap-4">
-				<div className="flex items-center justify-between gap-7">
-					<label className="text-lg font-bold text-stone-600">Name</label>
-					<input
-						type="text"
-						required
-						className="h-8 w-40 rounded-2xl px-4"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-					/>
+			<form
+				className="col-span-full flex h-2/5 flex-col items-center justify-evenly gap-4 rounded-2xl bg-stone-200 p-5"
+				onSubmit={handleSubmit}
+			>
+				<div className="flex flex-col gap-4">
+					<div className="flex items-center justify-between gap-7">
+						<label className="text-lg font-bold text-stone-600">Name</label>
+						<input
+							type="text"
+							required
+							className="h-8 w-40 rounded-2xl px-4"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+					<div className="flex items-center justify-between gap-7">
+						<label className="text-lg font-bold text-stone-600">Amount</label>
+						<input
+							type="number"
+							required
+							className="h-8 w-40 rounded-2xl px-4"
+							value={billAmount}
+							onChange={(e) => setBillAmount(e.target.value)}
+						/>
+					</div>
+					<div className="flex justify-evenly">
+						<Button type="form">Submit</Button>
+						<Button
+							type="cancel"
+							onClick={handleCancel}
+						>
+							Cancel
+						</Button>
+					</div>
 				</div>
-				<div className="flex items-center justify-between gap-7">
-					<label className="text-lg font-bold text-stone-600">Amount</label>
-					<input
-						type="number"
-						required
-						className="h-8 w-40 rounded-2xl px-4"
-						value={billAmount}
-						onChange={(e) => setBillAmount(e.target.value)}
-					/>
-				</div>
-				<div className="flex justify-evenly">
-					<Button type="form">Submit</Button>
-					<Button
-						type="cancel"
-						onClick={handleCancel}
-					>
-						Cancel
-					</Button>
-				</div>
-			</div>
-		</form>
+			</form>
+		</>
 	);
 }
