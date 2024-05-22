e7fc73cb030212a32a1827bbef79fec5fb36153d
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 07:43:31 +0200

    feat: Get dynamic bill amount

--- src/components/PriceTag.jsx
+++ src/components/PriceTag.jsx
@@ -1,5 +1,7 @@
-export default function PriceTag() {
+export default function PriceTag({ amount }) {
 	return (
-		<p className="rounded-full bg-indigo-300 px-6 py-3 text-indigo-600">R200</p>
+		<p className="rounded-full bg-indigo-300 px-6 py-3 text-indigo-600">
+			R{amount}
+		</p>
 	);
 }
