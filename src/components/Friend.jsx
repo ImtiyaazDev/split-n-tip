fdb3d4cc0d5824b0e83ae8c5d6d27e0bab50f93d
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 10:03:08 +0200

    feat: Create function to capitalise first letter of user name

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,10 +1,17 @@
 import PriceTag from "./PriceTag";
 
-export default function Friend({amount, name}) {
+export default function Friend({ amount, name }) {
+	function capitaliseFirstLetter(string) {
+		if (!string) return "";
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
 	return (
 		<li className="flex items-center justify-between border-b border-b-stone-400 py-2.5">
 			<div>
-				<p className="text-xl font-bold text-stone-700">{name}</p>
+				<p className="text-xl font-bold text-stone-700">
+					{capitaliseFirstLetter(name)}
+				</p>
 				<p className="text-stone-500">The cost of meal:</p>
 			</div>
 			<PriceTag amount={amount} />
