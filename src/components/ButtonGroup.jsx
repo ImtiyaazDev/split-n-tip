57dce639c46f28f88f89aa5ade178320f2e4b8f4
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 07:45:32 +0200

    feat: Add event handler to add a new user

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { toggleModal } from "../slices/modalSlice";
+import { addUser } from "../slices/billSlice";
 import Button from "./Button";
 
 export default function ButtonGroup() {
@@ -7,11 +8,17 @@ export default function ButtonGroup() {
 
 	const handleClick = () => {
 		dispatch(toggleModal(true));
+		dispatch(addUser());
 	};
 
 	return (
 		<div className="relative row-start-1 row-end-1 flex items-start justify-between">
-			<Button type="primary" onClick={handleClick}>Add Payee</Button>
+			<Button
+				type="primary"
+				onClick={handleClick}
+			>
+				Add Payee
+			</Button>
 			<Button type="secondary">New Bill</Button>
 		</div>
 	);
