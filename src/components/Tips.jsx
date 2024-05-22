bf88ebcf46e36ca560aa8177db10b505b24150e8
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 17:53:00 +0200

    style: Add borders to each group and increase padding

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -32,8 +32,8 @@ export default function Tips() {
 	);
 
 	return (
-		<div className="flex h-fit flex-col gap-4 rounded-2xl bg-stone-200 p-5">
-			<form className="flex items-center justify-between">
+		<div className="flex h-fit flex-col rounded-2xl bg-stone-200 p-5">
+			<form className="flex items-center justify-between border-b border-b-stone-400 py-2.5">
 				<label className="text-xl font-bold text-stone-700">Tip (%)</label>
 				<input
 					type="number"
@@ -45,11 +45,11 @@ export default function Tips() {
 					max="100"
 				/>
 			</form>
-			<div className="flex items-center justify-between">
+			<div className="flex items-center justify-between border-b border-b-stone-400 py-2.5">
 				<p className="text-xl font-bold text-stone-700">Tip Amount</p>
 				<PriceTag amount={formatToTwoDigits(tipAmount)} />
 			</div>
-			<div className="flex items-center justify-between">
+			<div className="flex items-center justify-between py-2.5">
 				<p className="text-xl font-bold text-stone-700">
 					Tip Amount per person
 				</p>
