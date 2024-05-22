91685041d86849c669e0e5ef57384ccad8e74cf8
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 12:08:27 +0200

    feat: Conditionally render friendslist only when users are added

--- src/components/FriendMeals.jsx
+++ src/components/FriendMeals.jsx
@@ -5,10 +5,11 @@ import PriceTag from "./PriceTag";
 
 export default function FriendMeals() {
 	const billAmount = useSelector((store) => store.bill.billAmount);
+	const users = useSelector((store) => store.bill.users);
 
 	return (
 		<div className="space-y-8 rounded-2xl bg-stone-200 p-5">
-			<FriendsList />
+			{users.length !== 0 && <FriendsList />}
 			<div className="flex items-center justify-between">
 				<p className="text-2xl font-bold text-violet-600">Bill Amount</p>
 				<PriceTag amount={billAmount} />
