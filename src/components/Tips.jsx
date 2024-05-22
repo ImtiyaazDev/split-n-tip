3ef0e3f59e5c53c1216a099d17c95f77ad06c3bb
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 11:06:57 +0200

    refactor: Use redux to create controlled element to update tip percentage

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import PriceTag from "./PriceTag";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { updateTipPercent } from "../slices/billSlice";
 
 export default function Tips() {
-	const [tipPercent, setTipPercent] = useState(10);
 	const billAmount = useSelector((store) => store.bill.billAmount);
+	const tipPercent = useSelector((store) => store.bill.tipPercent);
 	const tipAmount = (tipPercent / 100) * billAmount;
 	const users = useSelector((store) => store.bill.users);
 	const tipAmountPerPerson = tipAmount / users.length;
+	const dispatch = useDispatch();
+
+	const handleChange = (e) => {
+		dispatch(updateTipPercent(e.target.value));
+	};
 
 	return (
 		<div className="flex h-fit flex-col gap-3 rounded-2xl bg-stone-200 p-5">
@@ -18,7 +24,7 @@ export default function Tips() {
 					className="w-20 rounded-full bg-indigo-300 p-3 text-center text-indigo-600 outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 					placeholder="5"
 					defaultValue={tipPercent}
-					onChange={(e) => setTipPercent(Number(e.target.value))}
+					onChange={handleChange}
 					min="0"
 					max="100"
 				/>
