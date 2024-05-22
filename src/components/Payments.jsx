ef8fbcf4ad107f2f88eb927d05e9b742b7da55fc
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-22 12:29:45 +0200

    feat: Conditionally render a message when there is no split payment

--- src/components/Payments.jsx
+++ src/components/Payments.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import PriceTag from "./PriceTag";
 import SinglePayment from "./SinglePayment";
+import Message from "./Message";
 
 export default function Payments() {
 	const users = useSelector((store) => store.bill.users);
@@ -12,19 +13,25 @@ export default function Payments() {
 
 	return (
 		<div className="h-fit space-y-6 rounded-2xl bg-violet-200 p-5">
-			<ul>
-				{users.map((user) => (
-					<SinglePayment
-						key={user.id}
-						name={user.name}
-						amount={user.amount + tipAmountPerPerson}
-					/>
-				))}
-			</ul>
-			<div className="flex items-center justify-between">
-				<p className="text-2xl font-bold text-violet-600">Total</p>
-				<PriceTag amount={tipAmount + billAmount} />
-			</div>
+			{users.length === 0 ? (
+				<Message message="Add a new bill..." />
+			) : (
+				<>
+					<ul>
+						{users.map((user) => (
+							<SinglePayment
+								key={user.id}
+								name={user.name}
+								amount={user.amount + tipAmountPerPerson}
+							/>
+						))}
+					</ul>
+					<div className="flex items-center justify-between">
+						<p className="text-2xl font-bold text-violet-600">Total</p>
+						<PriceTag amount={tipAmount + billAmount} />
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
