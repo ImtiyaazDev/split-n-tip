a6974416e77490088bc3228441dc84449e1a4aaa
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-23 09:58:32 +0200

    style: Add animation to main heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,3 +1,3 @@
 export default function Heading() {
-	return <h1 className="text-4xl font-heading">Split-n-Tip</h1>;
+	return <h1 className="animate-glow font-heading text-4xl text-violet-500">Split-n-Tip</h1>;
 }
