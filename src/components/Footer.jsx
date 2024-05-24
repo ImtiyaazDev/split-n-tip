a41ba2c392bfd3cfea9fdb22f7d88bf078942458
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-05-24 11:53:29 +0200

    stlye: Add focus state to anchor tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 				<a
 					href="https://imtiyaazdev.vercel.app/"
 					target="_blank"
-					className="transition-colors duration-300 hover:text-violet-500"
+					className="rounded-full outline-none transition-colors duration-300 hover:text-violet-500 focus:ring-2 focus:ring-violet-500 focus:ring-offset-2"
 				>
 					Imtiyaaz Isaacs
 				</a>
